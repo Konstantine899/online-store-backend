8055567576fa3c79539c7eb0dd1dc37e6f32c271
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-02-25 17:16:25 +0300

    feat: Подключился к бд

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,19 @@
 import { Module } from '@nestjs/common';
-
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'Konstantine899',
+      password: '4343',
+      database: 'online-store',
+      models: [],
+      autoLoadModels: true, // автоматическая загрузка моделей
+    }),
+  ],
   controllers: [],
   providers: [],
 })
