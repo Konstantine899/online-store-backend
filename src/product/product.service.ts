b374a54c4742c1007ef49398c778463647efb3a0
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-02-27 14:52:34 +0300

    feat: ProductController создал endpoint getOne и в ProductService создал метод findOneProduct

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/sequelize';
 import { ProductModel } from './product.model';
 import { CreateProductDto } from './dto/create-product.dto';
@@ -18,4 +18,17 @@ export class ProductService {
     const fileName = await this.fileService.createFile(image);
     return await this.productRepository.create({ ...dto, image: fileName });
   }
+
+  async findOneProduct(id: string): Promise<ProductModel> {
+    const product = await this.productRepository.findByPk(id);
+    if (!product) {
+      throw new HttpException(
+        'По вашему запросу ничего не найдено',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return product;
+  }
+
+  async findAllProducts() {}
 }
