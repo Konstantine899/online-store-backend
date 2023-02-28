927364e1ac5919d031100abda1d47ef08af7b95d
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-02-28 16:42:03 +0300

    refactor: file.service.ts формирую filePath в зависимости от режима сборки

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -2,6 +2,7 @@ import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import * as path from 'path';
 import * as fs from 'fs';
 import * as uuid from 'uuid';
+import * as process from 'process';
 
 @Injectable()
 export class FileService {
@@ -9,7 +10,10 @@ export class FileService {
     try {
       const extension = file.originalname.split('.').filter(Boolean).splice(1); // Достаю расширение файла
       const newFileName = `${uuid.v4()}.${extension}`; // генерирую новое уникальное имя имя файла
-      const filePath = path.resolve(__dirname, '..', 'static'); // получаю путь к директории где храняться статические файлы
+      const filePath =
+        process.env.NODE_ENV === 'development'
+          ? path.resolve(__dirname, '..', '..', 'static')
+          : path.resolve(__dirname, '..', 'static'); // получаю путь к директории где храняться статические файлы
       /*Проверяю если директория хранения статических файлов не существует, то создаю ее*/
       if (!fs.existsSync(filePath)) {
         fs.mkdirSync(filePath, { recursive: true }); // создаю директорию
@@ -26,7 +30,10 @@ export class FileService {
 
   async removeFile(fileName: string): Promise<boolean> {
     try {
-      const filePath = path.resolve(__dirname, '..', 'static');
+      const filePath =
+        process.env.NODE_ENV === 'development'
+          ? path.resolve(__dirname, '..', '..', 'static')
+          : path.resolve(__dirname, '..', 'static'); // получаю путь к директории где храняться статические файлы
       fs.rmSync(path.join(filePath, fileName));
       return true;
     } catch (error) {
@@ -43,7 +50,10 @@ export class FileService {
   ): Promise<string> {
     try {
       // Удаляю старый файл
-      const filePath = path.resolve(__dirname, '..', 'static');
+      const filePath =
+        process.env.NODE_ENV === 'development'
+          ? path.resolve(__dirname, '..', '..', 'static')
+          : path.resolve(__dirname, '..', 'static'); // получаю путь к директории где храняться статические файлы
       fs.rmSync(path.join(filePath, oldFile));
 
       // генерирую новый файл
