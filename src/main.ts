9bb1d809f49608d5107da6829e909a218dbfd4a2
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-03-02 14:17:03 +0300

    feat: CustomValidationPipe использую глобально во всем приложении. CreateProductDto поле price явно вреобразую к number

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as process from 'process';
+import { CustomValidationPipe } from './pipes/custom-validation-pipe';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('online-store');
-
+  app.useGlobalPipes(...[new CustomValidationPipe()]);
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
 
