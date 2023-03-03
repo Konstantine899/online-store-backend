da87db6e07527ab33e05b1f6725e74f12c9c0119
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-03-03 15:44:42 +0300

    feat: UniqueConstraintException

---
+++ src/exceptions/sequelize-unique-constraint.exception.ts
@@ -0,0 +1,26 @@
+import { UniqueConstraintError } from 'sequelize';
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(UniqueConstraintError)
+export class SequelizeUniqueConstraintException implements ExceptionFilter {
+  catch(exception: UniqueConstraintError, host: ArgumentsHost) {
+    const context = host.switchToHttp();
+    const response = context.getResponse<Response>();
+    const request = context.getRequest<Request>();
+    const { url } = request;
+    const { name } = exception;
+    const errorResponse = {
+      path: url,
+      timestamp: new Date().toISOString(),
+      message: name,
+    };
+
+    response.status(HttpStatus.BAD_REQUEST).json(errorResponse);
+  }
+}
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -9,6 +9,7 @@ import {
   Post,
   Put,
   UploadedFile,
+  UseFilters,
   UseInterceptors,
   UsePipes,
   ValidationPipe,
@@ -17,30 +18,32 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { ProductService } from './product.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { ProductModel } from './product.model';
+import { SequelizeUniqueConstraintException } from '../exceptions/sequelize-unique-constraint.exception';
 
 @Controller('product')
+@UseFilters(SequelizeUniqueConstraintException)
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
   @Post('/create')
   @HttpCode(201)
   @UseInterceptors(FileInterceptor('image'))
   async create(
-	@Body() dto: CreateProductDto,
-	@UploadedFile() image: Express.Multer.File,
+    @Body() dto: CreateProductDto,
+    @UploadedFile() image: Express.Multer.File,
   ): Promise<ProductModel> {
-	return this.productService.productCreate(dto, image);
+    return this.productService.productCreate(dto, image);
   }
 
   @HttpCode(200)
   @Get('/getone/:id([0-9]+)')
   async getOne(@Param('id', ParseIntPipe) id: number): Promise<ProductModel> {
-	return this.productService.findOneProduct(id);
+    return this.productService.findOneProduct(id);
   }
 
   @HttpCode(200)
   @Get('/getall')
   async getAll(): Promise<ProductModel[]> {
-	return this.productService.findAllProducts();
+    return this.productService.findAllProducts();
   }
 
   @Put('/update/:id([0-9]+)')
@@ -48,16 +51,16 @@ export class ProductController {
   @UsePipes(ValidationPipe)
   @UseInterceptors(FileInterceptor('image'))
   async update(
-	@Param('id', ParseIntPipe) id: number,
-	@Body() dto: CreateProductDto,
-	@UploadedFile() image: Express.Multer.File,
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: CreateProductDto,
+    @UploadedFile() image: Express.Multer.File,
   ): Promise<ProductModel> {
-	return this.productService.updateProduct(id, dto, image);
+    return this.productService.updateProduct(id, dto, image);
   }
 
   @HttpCode(200)
   @Delete('/delete/:id([0-9]+)')
   async delete(@Param('id', ParseIntPipe) id: number): Promise<boolean> {
-	return this.productService.removeProduct(id);
+    return this.productService.removeProduct(id);
   }
 }
