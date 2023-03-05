5051f873b95148bb80d2f774255f5df6d7071260
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-03-05 22:45:26 +0300

    refactor: CustomValidationPipe

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -3,14 +3,14 @@ import { Transform } from 'class-transformer';
 
 export class CreateProductDto {
   @IsNotEmpty({ message: 'Имя не должно быть пустым' })
-  @IsString({ message: 'Имя продукта должно быть строкой' })
+  @IsString({ message: 'Имя  должно быть строкой' })
   readonly name: string;
 
   @IsNumber(
-	{ allowNaN: false, maxDecimalPlaces: 2 },
-	{
-		message: 'Цена продукта должна быть числом c двумя знаками после точки',
-	},
+    { allowNaN: false, maxDecimalPlaces: 2 },
+    {
+      message: 'Цена продукта должна быть числом c двумя знаками после точки',
+    },
   )
   @Transform(({ value }): number => Number.parseFloat(value))
   readonly price: number;
