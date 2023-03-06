bf87494746b3f1e41f3f03b379448e055bad43f0
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-03-06 16:34:41 +0300

    feat: Добавил CategoryModel описал CRUD опрации

---
+++ src/category/category.service.ts
@@ -0,0 +1,76 @@
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { CategoryModel } from './category-model';
+import { CreateCategoryDto } from './dto/create-category.dto';
+
+@Injectable()
+export class CategoryService {
+  constructor(
+	@InjectModel(CategoryModel)
+	private categoryRepository: typeof CategoryModel,
+  ) {}
+
+  async create(dto: CreateCategoryDto) {
+	const category = await this.categoryRepository.create(dto);
+	if (!category) {
+		throw new HttpException(
+		'Не предвиденная ошибка',
+		HttpStatus.INTERNAL_SERVER_ERROR,
+		);
+	}
+	return category;
+  }
+
+  async findAll(): Promise<CategoryModel[]> {
+	const categories = await this.categoryRepository.findAll();
+	if (!categories) {
+		throw new NotFoundException('Не найдено');
+	}
+	return categories;
+  }
+
+  async findOne(id: number): Promise<CategoryModel> {
+	const category = await this.categoryRepository.findByPk(id);
+	if (!category) {
+		throw new NotFoundException('Не найдено');
+	}
+	return category;
+  }
+
+  async update(id: number, dto: CreateCategoryDto): Promise<CategoryModel> {
+	const category = await this.categoryRepository.findByPk(id);
+	if (!category) {
+		throw new NotFoundException('Не найдено');
+	}
+	const findCategory = await category.update({ ...dto, name: dto.name });
+	if (!findCategory) {
+		throw new HttpException(
+		'Внутренняя ошибка сервера',
+		HttpStatus.INTERNAL_SERVER_ERROR,
+		);
+	}
+	return findCategory;
+  }
+
+  async remove(id: number): Promise<boolean> {
+	const category = await this.categoryRepository.findByPk(id);
+	if (!category) {
+		throw new NotFoundException('Не найдено');
+	}
+	const removeCategory = await this.categoryRepository.destroy({
+		where: { id },
+	});
+	if (!removeCategory) {
+		throw new HttpException(
+		'Внутренняя ошибка сервера',
+		HttpStatus.INTERNAL_SERVER_ERROR,
+		);
+	}
+	return true;
+  }
+}
