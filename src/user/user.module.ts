f628fefc5eede6122036fd280040cc052b16d602
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-03-09 15:29:45 +0300

    feat: Добавил модуль RoleModule и описал оссациацию Many-to-Many таблиц UserModel и RoleModel через промежуточную таблицу UserRoleModel

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,10 +3,11 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UserModel } from './user.model';
+import { RoleModel } from '../role/role.model';
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
-  imports: [SequelizeModule.forFeature([UserModel])],
+  imports: [SequelizeModule.forFeature([UserModel, RoleModel])],
 })
 export class UserModule {}
