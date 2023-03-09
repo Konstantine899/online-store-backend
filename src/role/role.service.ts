7d83dba89d674d83e147a031187973e960e4bff9
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-03-09 16:53:27 +0300

    feat: Создание и получение роли в модуле RoleModule присвоение роли при создании user

--- src/role/role.service.ts
+++ src/role/role.service.ts
@@ -1,4 +1,35 @@
-import { Injectable } from '@nestjs/common';
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { RoleModel } from './role.model';
+import { CreateRoleDto } from './dto/create-role.dto';
 
 @Injectable()
-export class RoleService {}
+export class RoleService {
+  constructor(
+    @InjectModel(RoleModel) private roleRepository: typeof RoleModel,
+  ) {}
+
+  async create(dto: CreateRoleDto): Promise<RoleModel> {
+    const role = await this.roleRepository.create(dto);
+    if (!role) {
+      throw new HttpException(
+        'Произошла не предвиденная ошибка',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+    return role;
+  }
+
+  async findOne(value: string): Promise<RoleModel> {
+    const role = await this.roleRepository.findOne({ where: { value } });
+    if (!role) {
+      throw new NotFoundException('Не найдено');
+    }
+    return role;
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -7,21 +7,25 @@ import {
 import { InjectModel } from '@nestjs/sequelize';
 import { UserModel } from './user.model';
 import { CreateUserDto } from './dto/create-user.dto';
+import { RoleService } from '../role/role.service';
 
 @Injectable()
 export class UserService {
   constructor(
 	@InjectModel(UserModel) private userRepository: typeof UserModel,
+	private roleService: RoleService,
   ) {}
 
   async create(dto: CreateUserDto): Promise<UserModel> {
 	const user = await this.userRepository.create(dto);
+	const role = await this.roleService.findOne('USER');
 	if (!user) {
 		throw new HttpException(
 		'Произошла не предвиденная ошибка',
 		HttpStatus.INTERNAL_SERVER_ERROR,
 		);
 	}
+	await user.$set('roles', [role.id]); // #set перезаписываю поле
 	return user;
   }
 
@@ -34,7 +38,7 @@ export class UserService {
   }
 
   async findAll(): Promise<UserModel[]> {
-	const users = await this.userRepository.findAll();
+	const users = await this.userRepository.findAll({ include: { all: true } });
 	if (!users) {
 		throw new NotFoundException('Не найдено');
 	}
@@ -43,6 +47,7 @@ export class UserService {
 
   async update(id: number, dto: CreateUserDto): Promise<UserModel> {
 	const user = await this.findOne(id);
+	const role = await this.roleService.findOne('USER');
 	const updateUser = await user.update({
 		...dto,
 		email: dto.email,
@@ -54,6 +59,7 @@ export class UserService {
 		HttpStatus.INTERNAL_SERVER_ERROR,
 		);
 	}
+	await user.$set('roles', [role.id]); // #set перезаписываю поле
 	return updateUser;
   }
 
