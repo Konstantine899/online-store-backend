a153ed958c48c289bf66e5097395220d0e62d450
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-04-07 15:30:37 +0300

    feat: в cart.service.ts добавил приватный метод TransformData

--- src/cart/cart.service.ts
+++ src/cart/cart.service.ts
@@ -2,10 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { CartRepository } from './cart.repository';
 import { Request, Response } from 'express';
 import { CartModel } from './cart.model';
+import { ProductModel } from '../product/product.model';
 
 interface IParams {
   productId?: number;
   quantity?: number;
+}
+
+export interface ITransformData {
+  cartId: number;
+  products: ProductModel[];
 }
 
 @Injectable()
@@ -16,126 +22,147 @@ export class CartService {
   signed: boolean = true;
 
   public async getCart(
-    request: Request,
-    response: Response,
-  ): Promise<CartModel> {
-    let cart: CartModel;
-    const { cartId } = request.signedCookies;
-    if (cartId) {
-      cart = await this.cartRepository.findCart(cartId);
-    } else {
-      cart = await this.cartRepository.createCart();
-    }
+	request: Request,
+	response: Response,
+  ): Promise<ITransformData> {
+	let cart: CartModel;
+	const { cartId } = request.signedCookies;
+	if (cartId) {
+		cart = await this.cartRepository.findCart(cartId);
+	} else {
+		cart = await this.cartRepository.createCart();
+	}
 
-    response.cookie('cartId', cart.id, {
-      maxAge: this.maxAge,
-      signed: this.signed, // использую секретный ключ для подписки value
-    });
-    return cart;
+	response.cookie('cartId', cart.id, {
+		maxAge: this.maxAge,
+		signed: this.signed, // использую секретный ключ для подписки value
+	});
+	return this.TransformData(cart);
   }
 
   public async appendToCart(
-    request: Request,
-    response: Response,
-    params: IParams,
-  ): Promise<CartModel> {
-    let { cartId } = request.signedCookies;
-    const { productId, quantity } = params;
-    if (!cartId) {
-      const created = await this.cartRepository.createCart();
-      cartId = created.id;
-    }
-    const cart = await this.cartRepository.appendToCart(
-      cartId,
-      productId,
-      quantity,
-    );
-    response.cookie('cartId', cart.id, {
-      maxAge: this.maxAge,
-      signed: this.signed,
-    });
-    return cart;
+	request: Request,
+	response: Response,
+	params: IParams,
+  ): Promise<ITransformData> {
+	let { cartId } = request.signedCookies;
+	const { productId, quantity } = params;
+	if (!cartId) {
+		const created = await this.cartRepository.createCart();
+		cartId = created.id;
+	}
+	const cart = await this.cartRepository.appendToCart(
+		cartId,
+		productId,
+		quantity,
+	);
+	response.cookie('cartId', cart.id, {
+		maxAge: this.maxAge,
+		signed: this.signed,
+	});
+	return this.TransformData(cart);
   }
 
   public async increment(
-    request: Request,
-    response: Response,
-    params: IParams,
-  ): Promise<CartModel> {
-    const { productId, quantity } = params;
-    let { cartId } = request.signedCookies;
-    if (!cartId) {
-      const created = await this.cartRepository.createCart();
-      cartId = created.id;
-    }
-    const cart = await this.cartRepository.increment(
-      cartId,
-      productId,
-      quantity,
-    );
-    response.cookie('cartId', cart.id, {
-      maxAge: this.maxAge,
-      signed: this.signed,
-    });
-    return cart;
+	request: Request,
+	response: Response,
+	params: IParams,
+  ): Promise<ITransformData> {
+	const { productId, quantity } = params;
+	let { cartId } = request.signedCookies;
+	if (!cartId) {
+		const created = await this.cartRepository.createCart();
+		cartId = created.id;
+	}
+	const cart = await this.cartRepository.increment(
+		cartId,
+		productId,
+		quantity,
+	);
+	response.cookie('cartId', cart.id, {
+		maxAge: this.maxAge,
+		signed: this.signed,
+	});
+	return this.TransformData(cart);
   }
 
   public async decrement(
-    request: Request,
-    response: Response,
-    params: IParams,
-  ): Promise<CartModel> {
-    const { productId, quantity } = params;
-    let { cartId } = request.signedCookies;
-    if (!cartId) {
-      const created = await this.cartRepository.createCart();
-      cartId = created.id;
-    }
-    const cart = await this.cartRepository.decrement(
-      cartId,
-      productId,
-      quantity,
-    );
-    response.cookie('cartId', cart.id, {
-      maxAge: this.maxAge,
-      signed: this.signed,
-    });
-    return cart;
+	request: Request,
+	response: Response,
+	params: IParams,
+  ): Promise<ITransformData> {
+	const { productId, quantity } = params;
+	let { cartId } = request.signedCookies;
+	if (!cartId) {
+		const created = await this.cartRepository.createCart();
+		cartId = created.id;
+	}
+	const cart = await this.cartRepository.decrement(
+		cartId,
+		productId,
+		quantity,
+	);
+	response.cookie('cartId', cart.id, {
+		maxAge: this.maxAge,
+		signed: this.signed,
+	});
+	return this.TransformData(cart);
   }
 
   public async removeProductFromCart(
-    request,
-    response,
-    params: IParams,
-  ): Promise<CartModel> {
-    const { productId } = params;
-    let { cartId } = request.signedCookies;
-    if (!cartId) {
-      const created = await this.cartRepository.createCart();
-      cartId = created.id;
-    }
-    const cart = await this.cartRepository.removeFromCart(cartId, productId);
-    response.cookie('cartId', cart.id, {
-      maxAge: this.maxAge,
-      signed: this.signed,
-    });
-    return cart;
+	request,
+	response,
+	params: IParams,
+  ): Promise<ITransformData> {
+	const { productId } = params;
+	let { cartId } = request.signedCookies;
+	if (!cartId) {
+		const created = await this.cartRepository.createCart();
+		cartId = created.id;
+	}
+	const cart = await this.cartRepository.removeFromCart(cartId, productId);
+	response.cookie('cartId', cart.id, {
+		maxAge: this.maxAge,
+		signed: this.signed,
+	});
+	return this.TransformData(cart);
   }
 
   public async clearCart(
-    request: Request,
-    response: Response,
-  ): Promise<CartModel> {
-    let { cartId } = request.signedCookies;
-    if (!cartId) {
-      const created = await this.cartRepository.createCart();
-      cartId = created.id;
-    }
-    const cart = await this.cartRepository.clearCart(cartId);
-    response.cookie('cartId', cart.id, {
-      maxAge: this.maxAge,
-      signed: this.signed,
-    });
-    return cart;
+	request: Request,
+	response: Response,
+  ): Promise<ITransformData> {
+	let { cartId } = request.signedCookies;
+	if (!cartId) {
+		const created = await this.cartRepository.createCart();
+		cartId = created.id;
+	}
+	const cart = await this.cartRepository.clearCart(cartId);
+	response.cookie('cartId', cart.id, {
+		maxAge: this.maxAge,
+		signed: this.signed,
+	});
+	return this.TransformData(cart);
+  }
+
+  private TransformData(cart: any): ITransformData {
+	const data: ITransformData = {
+		cartId: undefined,
+		products: undefined,
+	};
+	data.products = [];
+
+	data.cartId = cart.id;
+	if (cart.products) {
+		data.products = cart.products.map((item) => {
+		return {
+			productId: item.id,
+			name: item.name,
+			price: item.price,
+			quantity: item['CartProductModel'].quantity,
+		};
+		});
+	}
+	return data;
   }
 }
