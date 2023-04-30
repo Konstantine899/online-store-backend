99570699238663b7f56ed9947fa2f26c08eae00d
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-04-30 14:26:10 +0300

    fix: register login refresh token убрал открытое получение user

--- src/auth/decorators/documentation/registration.documentation.ts
+++ src/auth/decorators/documentation/registration.documentation.ts
@@ -18,23 +18,12 @@ export function RegistrationDocumentation() {
 		example: {
 			status: `success`,
 			data: {
-			user: {
-				id: 50,
-				email: 'test1@gmail.com',
-				roles: [
-				{
-					id: 2,
-					role: 'USER',
-					description: 'Пользователь',
-				},
-				],
-			},
 			payload: {
 				type: 'bearer',
 				accessToken:
-				'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTAsInJvbGVzIjpbeyJpZCI6Miwicm9sZSI6IlVTRVIiLCJkZXNjcmlwdGlvbiI6ItCf0L7Qu9GM0LfQvtCy0LDRgtC10LvRjCJ9XSwiaWF0IjoxNjgyNjg3ODk4LCJleHAiOjE2ODI3NzQyOTgsInN1YiI6IjUwIn0.vyUxpplAmZa-6sLGnI7HIMiBAJw6CPY9yh_25ZfqDHM',
+				'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6NTEsInJvbGVzIjpbeyJpZCI6Miwicm9sZSI6IlVTRVIiLCJkZXNjcmlwdGlvbiI6ItCf0L7Qu9GM0LfQvtCy0LDRgtC10LvRjCJ9XSwiaWF0IjoxNjgyODUzNTkxLCJleHAiOjE2ODI5Mzk5OTEsInN1YiI6IjUxIn0.swRpSMnpVymIUNibREskcfao11QxgibilYsJLVmkk88',
 				refreshToken:
-				'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2ODI2ODc4OTgsImV4cCI6MTY4Mjc3NDI5OCwic3ViIjoiNTAiLCJqdGkiOiI2MCJ9.6FfKlqEExNMTYHMfsM7Imcc_wEq2zCIS4rjgcEVa4y0',
+				'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE2ODI4NTM1OTEsImV4cCI6MTY4MjkzOTk5MSwic3ViIjoiNTEiLCJqdGkiOiI2NSJ9.rgyg069iM_88PzF_ns9S_U1uC6abCN1axxn30Ro05Xo',
 			},
 			},
 		},
