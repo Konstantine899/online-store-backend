4883d8f0f7e0c9202253acfc31ea21194eeafc85
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-05-01 17:48:41 +0300

    feat: role.controller.ts добавил проверки авторизации и аутентификации

--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -1,9 +1,20 @@
-import { Body, Controller, Get, HttpCode, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { RoleService } from './role.service';
 import { CreateRoleDto } from './dto/create-role.dto';
 import { RoleModel } from './role.model';
 import { ApiTags } from '@nestjs/swagger';
 import { CreateRoleDocumentation } from './decorators/create-role.documentation';
+import { Roles } from '../auth/decorators/roles-auth.decorator';
+import { JwtGuard } from '../token/jwt.guard';
+import { RoleGuard } from './role.guard';
 
 @ApiTags(`Роль`)
 @Controller('role')
@@ -12,20 +23,29 @@ export class RoleController {
 
   @CreateRoleDocumentation()
   @HttpCode(201)
+  @Roles('ADMIN')
+  @UseGuards(JwtGuard)
+  @UseGuards(RoleGuard)
   @Post(`/create`)
   public async createRole(@Body() dto: CreateRoleDto): Promise<RoleModel> {
-    return this.roleService.createRole(dto);
+	return this.roleService.createRole(dto);
   }
 
   @HttpCode(200)
+  @Roles('ADMIN')
+  @UseGuards(JwtGuard)
+  @UseGuards(RoleGuard)
   @Get('/one/:role')
   public async getOne(@Param('role') role: string): Promise<RoleModel> {
-    return this.roleService.findRole(role);
+	return this.roleService.findRole(role);
   }
 
   @HttpCode(200)
+  @Roles('ADMIN')
+  @UseGuards(JwtGuard)
+  @UseGuards(RoleGuard)
   @Get('/all')
   public async getAllRoles(): Promise<RoleModel[]> {
-    return this.roleService.getAllRoles();
+	return this.roleService.getAllRoles();
   }
 }
