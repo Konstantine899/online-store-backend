0f407d90574e8e08451a0cc4120757d45b92fc43
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-05-02 12:57:40 +0300

    docs: модуль role в описание endpoints добавил ApiHeaders

--- src/role/decorators/all-roles-documentation.ts
+++ src/role/decorators/all-roles-documentation.ts
@@ -1,5 +1,6 @@
 import { applyDecorators, HttpStatus } from '@nestjs/common';
 import {
+  ApiHeaders,
   ApiNotFoundResponse,
   ApiOperation,
   ApiResponse,
@@ -8,6 +9,14 @@ import {
 export function AllRolesDocumentation() {
   return applyDecorators(
 	ApiOperation({ summary: `Список ролей` }),
+	ApiHeaders([
+		{
+		name: `Authorization`,
+		description:
+			'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZXMiOlt7ImlkIjoxLCJyb2xlIjoiQURNSU4iLCJkZXNjcmlwdGlvbiI6ItCQ0LTQvNC40L3QuNGB0YLRgNCw0YLQvtGAIn0seyJpZCI6Miwicm9sZSI6IlVTRVIiLCJkZXNjcmlwdGlvbiI6ItCf0L7Qu9GM0LfQvtCy0LDRgtC10LvRjCJ9XSwiaWF0IjoxNjgzMDA4MDg0LCJleHAiOjE2ODMwOTQ0ODQsInN1YiI6IjEifQ.u0CmxeLT6CEUg4Yx38FwBDZwQ5QUXABK5306OrEMNt0',
+		required: true,
+		},
+	]),
 	ApiResponse({
 		status: HttpStatus.OK,
 		schema: {
--- src/role/decorators/create-role.documentation.ts
+++ src/role/decorators/create-role.documentation.ts
@@ -1,10 +1,23 @@
 import { applyDecorators, HttpStatus } from '@nestjs/common';
-import { ApiBody, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import {
+  ApiBody,
+  ApiHeaders,
+  ApiOperation,
+  ApiResponse,
+} from '@nestjs/swagger';
 import { CreateRoleDto } from '../dto/create-role.dto';
 
 export function CreateRoleDocumentation() {
   return applyDecorators(
 	ApiOperation({ summary: `Создание роли пользователя` }),
+	ApiHeaders([
+		{
+		name: `Authorization`,
+		description:
+			'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZXMiOlt7ImlkIjoxLCJyb2xlIjoiQURNSU4iLCJkZXNjcmlwdGlvbiI6ItCQ0LTQvNC40L3QuNGB0YLRgNCw0YLQvtGAIn0seyJpZCI6Miwicm9sZSI6IlVTRVIiLCJkZXNjcmlwdGlvbiI6ItCf0L7Qu9GM0LfQvtCy0LDRgtC10LvRjCJ9XSwiaWF0IjoxNjgzMDA4MDg0LCJleHAiOjE2ODMwOTQ0ODQsInN1YiI6IjEifQ.u0CmxeLT6CEUg4Yx38FwBDZwQ5QUXABK5306OrEMNt0',
+		required: true,
+		},
+	]),
 	ApiBody({
 		description: `Структура входных данных для создания роли пользователя`,
 		type: CreateRoleDto,
