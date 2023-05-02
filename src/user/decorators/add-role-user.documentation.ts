89b1899c26acf4f654490eac4e47c073d832ec21
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-05-02 12:44:26 +0300

    docs: добавил  ApiHeaders в нужные endpoint

--- src/user/decorators/add-role-user.documentation.ts
+++ src/user/decorators/add-role-user.documentation.ts
@@ -3,6 +3,7 @@ import {
   ApiBody,
   ApiConflictResponse,
   ApiCreatedResponse,
+  ApiHeaders,
   ApiNotFoundResponse,
   ApiOperation,
 } from '@nestjs/swagger';
@@ -11,6 +12,14 @@ import { AddRoleDto } from '../dto/add-role.dto';
 export function AddRoleUserDocumentation() {
   return applyDecorators(
 	ApiOperation({ summary: `Добавление роли пользователю` }),
+	ApiHeaders([
+		{
+		name: `Authorization`,
+		description:
+			'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZXMiOlt7ImlkIjoxLCJyb2xlIjoiQURNSU4iLCJkZXNjcmlwdGlvbiI6ItCQ0LTQvNC40L3QuNGB0YLRgNCw0YLQvtGAIn0seyJpZCI6Miwicm9sZSI6IlVTRVIiLCJkZXNjcmlwdGlvbiI6ItCf0L7Qu9GM0LfQvtCy0LDRgtC10LvRjCJ9XSwiaWF0IjoxNjgzMDA4MDg0LCJleHAiOjE2ODMwOTQ0ODQsInN1YiI6IjEifQ.u0CmxeLT6CEUg4Yx38FwBDZwQ5QUXABK5306OrEMNt0',
+		required: true,
+		},
+	]),
 	ApiBody({
 		type: AddRoleDto,
 		description: `Структура входных данных для добавления роли пользователю`,
