33747fcdefdae228e6497884547f1308bb0819fe
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-05-02 12:54:37 +0300

    docs: модуль auth в описание endpoints добавил ApiHeaders

--- src/auth/decorators/documentation/profile-user.documentation.ts
+++ src/auth/decorators/documentation/profile-user.documentation.ts
@@ -1,6 +1,7 @@
 import { applyDecorators, HttpStatus } from '@nestjs/common';
 import {
   ApiBody,
+  ApiHeaders,
   ApiNotFoundResponse,
   ApiOperation,
   ApiResponse,
@@ -10,6 +11,14 @@ import { GetProfileUserRequest } from '../../requests/get-profile-user.request';
 export function ProfileUserDocumentation() {
   return applyDecorators(
 	ApiOperation({ summary: `Профиль пользователя` }),
+	ApiHeaders([
+		{
+		name: `Authorization`,
+		description:
+			'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZXMiOlt7ImlkIjoxLCJyb2xlIjoiQURNSU4iLCJkZXNjcmlwdGlvbiI6ItCQ0LTQvNC40L3QuNGB0YLRgNCw0YLQvtGAIn0seyJpZCI6Miwicm9sZSI6IlVTRVIiLCJkZXNjcmlwdGlvbiI6ItCf0L7Qu9GM0LfQvtCy0LDRgtC10LvRjCJ9XSwiaWF0IjoxNjgzMDA4MDg0LCJleHAiOjE2ODMwOTQ0ODQsInN1YiI6IjEifQ.u0CmxeLT6CEUg4Yx38FwBDZwQ5QUXABK5306OrEMNt0',
+		required: true,
+		},
+	]),
 	ApiBody({
 		type: GetProfileUserRequest,
 		description: `Это описание тела запроса в котором содержится пользователь т.е. request.user`,
