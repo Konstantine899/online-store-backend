181b19be6b0c5f5af1219c3746479f91a8b4958b
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-05-03 11:51:20 +0300

    docs: Модуль role заменил ApiHeaders на ApiBearerAuth

--- src/role/decorators/get-role.documentation.ts
+++ src/role/decorators/get-role.documentation.ts
@@ -1,6 +1,6 @@
 import { applyDecorators, HttpStatus } from '@nestjs/common';
 import {
-  ApiHeaders,
+  ApiBearerAuth,
   ApiNotFoundResponse,
   ApiOperation,
   ApiParam,
@@ -10,14 +10,7 @@ import {
 export function GetRoleDocumentation() {
   return applyDecorators(
 	ApiOperation({ summary: `Получение роли` }),
-	ApiHeaders([
-		{
-		name: `Authorization`,
-		description:
-			'bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZXMiOlt7ImlkIjoxLCJyb2xlIjoiQURNSU4iLCJkZXNjcmlwdGlvbiI6ItCQ0LTQvNC40L3QuNGB0YLRgNCw0YLQvtGAIn0seyJpZCI6Miwicm9sZSI6IlVTRVIiLCJkZXNjcmlwdGlvbiI6ItCf0L7Qu9GM0LfQvtCy0LDRgtC10LvRjCJ9XSwiaWF0IjoxNjgzMDA4MDg0LCJleHAiOjE2ODMwOTQ0ODQsInN1YiI6IjEifQ.u0CmxeLT6CEUg4Yx38FwBDZwQ5QUXABK5306OrEMNt0',
-		required: true,
-		},
-	]),
+	ApiBearerAuth('JWT-auth'),
 	ApiParam({
 		name: `Роль`,
 		description: `Роль`,
