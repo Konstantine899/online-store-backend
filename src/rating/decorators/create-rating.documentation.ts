ecf29bbddc4538ab4f3055b74662745db0b6a3d6
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-05-03 11:54:51 +0300

    docs: Модуль auth заменил ApiHeaders на ApiBearerAuth

--- src/rating/decorators/create-rating.documentation.ts
+++ src/rating/decorators/create-rating.documentation.ts
@@ -12,65 +12,65 @@ import { UserRequest } from '../requests/user.request';
 
 export function CreateRatingDocumentation() {
   return applyDecorators(
-    ApiOperation({ summary: `Создание рейтинга` }),
-    ApiBearerAuth('JWT-auth'),
-    ApiParam({
-      name: `productId`,
-      type: `string`,
-      description: `Идентификатор продукта`,
-      required: true,
-    }),
-    ApiParam({
-      name: `rating`,
-      type: `string`,
-      description: `Рейтинг продукта`,
-      required: true,
-    }),
-    ApiBody({
-      type: UserRequest,
-      description: `Это описание тела запроса в котором содержится пользователь т.е. request.user`,
-      required: true,
-    }),
-    ApiResponse({
-      description: `Created rating`,
-      status: HttpStatus.CREATED,
-      schema: {
-        title: `Созданный рейтинг продукта`,
-        example: { userId: 1, productId: 1, rating: 5 },
-      },
-    }),
-    ApiNotFoundResponse({
-      description: `Not Found`,
-      status: HttpStatus.NOT_FOUND,
-      schema: {
-        title: `Не найдено`,
-        anyOf: [
-          {
-            example: {
-              statusCode: HttpStatus.NOT_FOUND,
-              message: 'Продукт не найден',
-            },
-          },
-          {
-            example: {
-              statusCode: HttpStatus.NOT_FOUND,
-              message: 'Пользователь не найден',
-            },
-          },
-        ],
-      },
-    }),
-    ApiBadRequestResponse({
-      description: `Bad Request`,
-      status: HttpStatus.BAD_REQUEST,
-      schema: {
-        title: `Оценка рейтинга ставится повторно`,
-        description: `В клиентской части блокируй возможность ставить повторно рейтинг продукта`,
-        example: {
-          status: HttpStatus.BAD_REQUEST,
-          message: 'Оценка рейтинга выми была выставлена ранее',
-        },
-      },
-    }),
+	ApiOperation({ summary: `Создание рейтинга` }),
+	ApiBearerAuth('JWT-auth'),
+	ApiParam({
+		name: `productId`,
+		type: `string`,
+		description: `Идентификатор продукта`,
+		required: true,
+	}),
+	ApiParam({
+		name: `rating`,
+		type: `string`,
+		description: `Рейтинг продукта`,
+		required: true,
+	}),
+	ApiBody({
+		type: UserRequest,
+		description: `Это описание тела запроса в котором содержится пользователь т.е. request.user`,
+		required: true,
+	}),
+	ApiResponse({
+		description: `Created rating`,
+		status: HttpStatus.CREATED,
+		schema: {
+		title: `Созданный рейтинг продукта`,
+		example: { userId: 1, productId: 1, rating: 5 },
+		},
+	}),
+	ApiNotFoundResponse({
+		description: `Not Found`,
+		status: HttpStatus.NOT_FOUND,
+		schema: {
+		title: `Не найдено`,
+		anyOf: [
+			{
+			example: {
+				statusCode: HttpStatus.NOT_FOUND,
+				message: 'Продукт не найден',
+			},
+			},
+			{
+			example: {
+				statusCode: HttpStatus.NOT_FOUND,
+				message: 'Пользователь не найден',
+			},
+			},
+		],
+		},
+	}),
+	ApiBadRequestResponse({
+		description: `Bad Request`,
+		status: HttpStatus.BAD_REQUEST,
+		schema: {
+		title: `Оценка рейтинга ставится повторно`,
+		description: `В клиентской части блокируй возможность ставить повторно рейтинг продукта`,
+		example: {
+			status: HttpStatus.BAD_REQUEST,
+			message: 'Оценка рейтинга выми была выставлена ранее',
+		},
+		},
+	}),
   );
 }
