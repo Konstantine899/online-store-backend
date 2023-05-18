9b8e1701d21368a7c656b20a3112dc74e8a2e1c5
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-05-18 15:05:38 +0300

    refactor: AddRoleUserDocumentation

--- src/user/decorators/add-role-user.documentation.ts
+++ src/user/decorators/add-role-user.documentation.ts
@@ -11,50 +11,59 @@ import { AddRoleDto } from '../dto/add-role.dto';
 
 export function AddRoleUserDocumentation() {
   return applyDecorators(
-	ApiOperation({ summary: `Добавление роли пользователю` }),
-	ApiBearerAuth('JWT-auth'),
-	ApiBody({
-		type: AddRoleDto,
-		description: `Структура входных данных для добавления роли пользователю`,
-	}),
-	ApiCreatedResponse({
-		description: `Add Role`,
-		status: HttpStatus.CREATED,
-		schema: {
-		title: `Добавленная роль`,
-		example: [{ id: 1, userId: 1, roleId: 1 }],
-		},
-	}),
-	ApiNotFoundResponse({
-		description: `Not Found`,
-		status: HttpStatus.NOT_FOUND,
-		schema: {
-		anyOf: [
-			{
-			title: 'Пользователь',
-			description: `Пользователь не найден в БД`,
-			example: {
-				statusCode: 404,
-				message: 'Пользователь не найден в БД',
-			},
-			},
-			{
-			title: 'Роль пользователя',
-			description: `Роль не найдена в БД`,
-			example: { statusCode: 404, message: 'Роль не найдена в БД' },
-			},
-		],
-		},
-	}),
-	ApiConflictResponse({
-		description: `Conflict`,
-		status: HttpStatus.CONFLICT,
-		schema: {
-		example: {
-			statusCode: 409,
-			message: 'Данному пользователю уже присвоена роль ADMIN',
-		},
-		},
-	}),
+    ApiOperation({ summary: `Добавление роли пользователю` }),
+    ApiBearerAuth('JWT-auth'),
+    ApiBody({
+      type: AddRoleDto,
+      description: `Структура входных данных для добавления роли пользователю`,
+    }),
+    ApiCreatedResponse({
+      description: `Add Role`,
+      status: HttpStatus.CREATED,
+      schema: {
+        title: `Добавленная роль`,
+        example: [{ id: 1, userId: 1, roleId: 1 }],
+      },
+    }),
+    ApiNotFoundResponse({
+      description: `Not Found`,
+      status: HttpStatus.NOT_FOUND,
+      schema: {
+        anyOf: [
+          {
+            title: 'Пользователь',
+            description: `Пользователь не найден в БД`,
+            example: {
+              statusCode: HttpStatus.NOT_FOUND,
+              url: '/online-store/user/role/add',
+              path: '/online-store/user/role/add',
+              name: 'NotFoundException',
+              message: 'Пользователь не найден в БД',
+            },
+          },
+          {
+            title: 'Роль пользователя',
+            description: `Роль не найдена в БД`,
+            example: {
+              statusCode: HttpStatus.NOT_FOUND,
+              url: '/online-store/user/role/add',
+              path: '/online-store/user/role/add',
+              name: 'NotFoundException',
+              message: 'Роль не найдена в БД',
+            },
+          },
+        ],
+      },
+    }),
+    ApiConflictResponse({
+      description: `Conflict`,
+      status: HttpStatus.CONFLICT,
+      schema: {
+        example: {
+          statusCode: HttpStatus.CONFLICT,
+          message: 'Данному пользователю уже присвоена роль ADMIN',
+        },
+      },
+    }),
   );
 }
