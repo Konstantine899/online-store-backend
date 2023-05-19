dc5e6b32bb6b3daf787c598c71fa4d350f88e04d
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-05-19 10:55:24 +0300

    refactor: В AddRoleUserDocumentation добавил ApiBadRequestResponse

--- src/user/decorators/add-role-user.documentation.ts
+++ src/user/decorators/add-role-user.documentation.ts
@@ -8,62 +8,88 @@ import {
   ApiOperation,
 } from '@nestjs/swagger';
 import { AddRoleDto } from '../dto/add-role.dto';
+import { ApiBadRequestResponse } from '@nestjs/swagger/dist/decorators/api-response.decorator';
 
 export function AddRoleUserDocumentation() {
   return applyDecorators(
-    ApiOperation({ summary: `Добавление роли пользователю` }),
-    ApiBearerAuth('JWT-auth'),
-    ApiBody({
-      type: AddRoleDto,
-      description: `Структура входных данных для добавления роли пользователю`,
-    }),
-    ApiCreatedResponse({
-      description: `Add Role`,
-      status: HttpStatus.CREATED,
-      schema: {
-        title: `Добавленная роль`,
-        example: [{ id: 1, userId: 1, roleId: 1 }],
-      },
-    }),
-    ApiNotFoundResponse({
-      description: `Not Found`,
-      status: HttpStatus.NOT_FOUND,
-      schema: {
-        anyOf: [
-          {
-            title: 'Пользователь',
-            description: `Пользователь не найден в БД`,
-            example: {
-              statusCode: HttpStatus.NOT_FOUND,
-              url: '/online-store/user/role/add',
-              path: '/online-store/user/role/add',
-              name: 'NotFoundException',
-              message: 'Пользователь не найден в БД',
-            },
-          },
-          {
-            title: 'Роль пользователя',
-            description: `Роль не найдена в БД`,
-            example: {
-              statusCode: HttpStatus.NOT_FOUND,
-              url: '/online-store/user/role/add',
-              path: '/online-store/user/role/add',
-              name: 'NotFoundException',
-              message: 'Роль не найдена в БД',
-            },
-          },
-        ],
-      },
-    }),
-    ApiConflictResponse({
-      description: `Conflict`,
-      status: HttpStatus.CONFLICT,
-      schema: {
-        example: {
-          statusCode: HttpStatus.CONFLICT,
-          message: 'Данному пользователю уже присвоена роль ADMIN',
-        },
-      },
-    }),
+	ApiOperation({ summary: `Добавление роли пользователю` }),
+	ApiBearerAuth('JWT-auth'),
+	ApiBody({
+		type: AddRoleDto,
+		description: `Структура входных данных для добавления роли пользователю`,
+	}),
+	ApiCreatedResponse({
+		description: `Add Role`,
+		status: HttpStatus.CREATED,
+		schema: {
+		title: `Добавленная роль`,
+		example: [{ id: 1, userId: 1, roleId: 1 }],
+		},
+	}),
+	ApiNotFoundResponse({
+		description: `Not Found`,
+		status: HttpStatus.NOT_FOUND,
+		schema: {
+		anyOf: [
+			{
+			title: 'Пользователь',
+			description: `Пользователь не найден в БД`,
+			example: {
+				statusCode: HttpStatus.NOT_FOUND,
+				url: '/online-store/user/role/add',
+				path: '/online-store/user/role/add',
+				name: 'NotFoundException',
+				message: 'Пользователь не найден в БД',
+			},
+			},
+			{
+			title: 'Роль пользователя',
+			description: `Роль не найдена в БД`,
+			example: {
+				statusCode: HttpStatus.NOT_FOUND,
+				url: '/online-store/user/role/add',
+				path: '/online-store/user/role/add',
+				name: 'NotFoundException',
+				message: 'Роль не найдена в БД',
+			},
+			},
+		],
+		},
+	}),
+	ApiConflictResponse({
+		description: `Conflict`,
+		status: HttpStatus.CONFLICT,
+		schema: {
+		example: {
+			statusCode: HttpStatus.CONFLICT,
+			message: 'Данному пользователю уже присвоена роль ADMIN',
+		},
+		},
+	}),
+	ApiBadRequestResponse({
+		description: `Bad Request`,
+		status: HttpStatus.BAD_REQUEST,
+		schema: {
+		title: `Валидация`,
+		anyOf: [
+			{
+			example: {
+				status: HttpStatus.BAD_REQUEST,
+				property: 'role',
+				messages: ['Укажите role пользователя'],
+				value: '',
+			},
+			},
+			{
+			example: {
+				status: HttpStatus.BAD_REQUEST,
+				property: 'role',
+				messages: ['Поле role должно быть строкой'],
+				value: 1,
+			},
+			},
+		],
+		},
+	}),
   );
 }
--- src/user/dto/add-role.dto.ts
+++ src/user/dto/add-role.dto.ts
@@ -4,7 +4,7 @@ import { ApiProperty } from '@nestjs/swagger';
 
 export class AddRoleDto {
   @ApiProperty({
-	example: `1`,
+	example: 1,
 	description: `Идентификатор пользователя`,
   })
   @Transform((value) => Number(value))
