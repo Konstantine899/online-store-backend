aef6f918ff9b847676024cc86b395b00fe0ceeb6
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-05-22 21:54:43 +0300

    refactor: CheckLoginDocumentation и LoginCheckRequest

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -20,63 +20,63 @@ import { ApiTags } from '@nestjs/swagger';
 import { RegistrationDocumentation } from './decorators/documentation/registration.documentation';
 import { LoginDocumentation } from './decorators/documentation/login.documentation';
 import { UpdateAccessTokenDocumentation } from './decorators/documentation/update-access-token.documentation';
-import { CheckLoginDocumentation } from './decorators/documentation/check-login.documentation';
+import { LoginCheckDocumentation } from './decorators/documentation/login-check.documentation';
 import { UserModel } from '../user/user.model';
-import { GetProfileUserRequest } from './requests/get-profile-user.request';
+import { LoginCheckRequest } from './requests/login-check.request';
 import { LogoutDocumentation } from './decorators/documentation/logout.documentation';
 
 @ApiTags(`Аутентификация`)
 @Controller('auth')
 export class AuthController {
   constructor(
-	private readonly authService: AuthService,
-	private readonly userService: UserService,
+    private readonly authService: AuthService,
+    private readonly userService: UserService,
   ) {}
 
   @RegistrationDocumentation()
   @HttpCode(201)
   @Post('/registration')
   public async registration(
-	@Body() dto: RegisterDto,
+    @Body() dto: RegisterDto,
   ): Promise<{ status: HttpStatus; data: IAuthPayload }> {
-	return this.authService.registration(dto);
+    return this.authService.registration(dto);
   }
   @LoginDocumentation()
   @HttpCode(200)
   @Post('/login')
   public async login(
-	@Body() dto: LoginDto,
+    @Body() dto: LoginDto,
   ): Promise<{ status: HttpStatus; data: IAuthPayload }> {
-	return this.authService.login(dto);
+    return this.authService.login(dto);
   }
 
   @UpdateAccessTokenDocumentation()
   @HttpCode(201)
   @Post('/refresh')
   public async updateAccessToken(
-	@Body() dto: RefreshDto,
+    @Body() dto: RefreshDto,
   ): Promise<{ status: HttpStatus; data: IAuthPayload }> {
-	return this.authService.updateAccessToken(dto.refreshToken);
+    return this.authService.updateAccessToken(dto.refreshToken);
   }
 
-  @CheckLoginDocumentation()
+  @LoginCheckDocumentation()
   @UseGuards(JwtGuard)
   @Get('/login-check')
   public async loginCheck(
-	@Req() request: Request,
+    @Req() request: Request,
   ): Promise<{ status: HttpStatus; data: UserModel }> {
-	const { id } = request.user as GetProfileUserRequest;
-	const foundUser = await this.userService.loginCheck(id);
-	return {
-		status: HttpStatus.OK,
-		data: foundUser,
-	};
+    const { id } = request.user as LoginCheckRequest;
+    const foundUser = await this.userService.loginCheck(id);
+    return {
+      status: HttpStatus.OK,
+      data: foundUser,
+    };
   }
 
   @LogoutDocumentation()
   @UseGuards(JwtGuard)
   @Delete('/logout')
   public async logout(@Req() request: Request, @Body() refresh: RefreshDto) {
-	return this.authService.logout(refresh, request);
+    return this.authService.logout(refresh, request);
   }
 }
