8607cda4d62588db6ef7dfd3397cb7e64e8fde8b
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-05-29 19:57:12 +0300

    feat: Добавил CORS

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ import { swaggerConfig } from './config/swagger.config';
 
 async function bootstrap() {
   const PORT = process.env.PORT || 5000;
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.setGlobalPrefix('online-store');
   app.useGlobalPipes(...[new CustomValidationPipe()]);
   app.useGlobalFilters(
@@ -20,6 +20,10 @@ async function bootstrap() {
 		new CustomNotFoundExceptionFilter(),
 	],
   );
+  app.enableCors({
+	credentials: true,
+	origin: [`http://localhost:5000/online-store`],
+  });
   app.use(cookieParser(process.env.COOKIE_PARSER_SECRET_KEY));
   swaggerConfig(app);
   await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
