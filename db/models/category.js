1e2d638ea2c70dd361893b9ba943e1efefefa748
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-06-04 15:55:48 +0300

    refactor: Миграции ассоциации brand.js category.js product.js

--- db/models/category.js
+++ db/models/category.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.belongsTo(models.product, { as: `products`, onDelete: `RESTRICT` });
+      this.hasMany(models.product, { as: `products`, onDelete: `RESTRICT` });
     }
   }
   category.init(
