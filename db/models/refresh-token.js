135a4145560db7b437d93aca62fd9afcec747c6f
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-06-04 16:44:10 +0300

    refactor: Миграции ассоциации refresh-token.js

--- db/models/refresh-token.js
+++ db/models/refresh-token.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      this.belongsTo(models.user);
+      this.belongsTo(models.user, { as: `user` });
     }
   }
   RefreshToken.init(
