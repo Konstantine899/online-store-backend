bc1306a7fe305be47c90c0b093ea0070e559727b
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-06-07 08:47:07 +0300

    feat: добавил транзакции в product-property.controller.ts

--- src/product-property/product-property.controller.ts
+++ src/product-property/product-property.controller.ts
@@ -9,6 +9,7 @@ import {
   Post,
   Put,
   UseGuards,
+  UseInterceptors,
 } from '@nestjs/common';
 import { CreateProductPropertyDto } from './dto/create-product-property.dto';
 import { ProductPropertyService } from './product-property.service';
@@ -26,6 +27,9 @@ import { GetProductPropertyResponse } from './responses/get-product-property.res
 import { GetListProductPropertyResponse } from './responses/get-list-product-property.response';
 import { UpdateProductPropertyResponse } from './decorators/update-product-property.response';
 import { RemoveProductPropertyResponse } from './responses/remove-product-property.response';
+import { TransactionInterceptor } from '../interceptors/transaction-interceptor';
+import { TransactionDecorator } from '../decorators/transaction-decorator';
+import { Transaction } from 'sequelize';
 
 @ApiTags(`Свойства продукта`)
 @Controller('product-property')
@@ -39,9 +43,11 @@ export class ProductPropertyController {
   @UseGuards(JwtGuard, RoleGuard)
   @HttpCode(201)
   @Post('/product_id/:productId([0-9]+)/create')
+  @UseInterceptors(TransactionInterceptor)
   public async createProductProperty(
 	@Param('productId', ParseIntPipe) productId: number,
 	@Body() dto: CreateProductPropertyDto,
+	@TransactionDecorator() transaction: Transaction,
   ): Promise<CreateProductPropertyResponse> {
 	return this.productPropertyService.createProductProperty(productId, dto);
   }
@@ -51,9 +57,11 @@ export class ProductPropertyController {
   @UseGuards(JwtGuard, RoleGuard)
   @HttpCode(200)
   @Get('/product_id/:productId([0-9]+)/get-property/:id([0-9]+)')
+  @UseInterceptors(TransactionInterceptor)
   public async getProductProperty(
 	@Param('productId', ParseIntPipe) productId: number,
 	@Param('id', ParseIntPipe) id: number,
+	@TransactionDecorator() transaction: Transaction,
   ): Promise<GetProductPropertyResponse> {
 	return this.productPropertyService.getProductProperty(productId, id);
   }
@@ -63,8 +71,10 @@ export class ProductPropertyController {
   @UseGuards(JwtGuard, RoleGuard)
   @HttpCode(200)
   @Get('/product_id/:productId([0-9]+)/properties')
+  @UseInterceptors(TransactionInterceptor)
   public async getListProductProperty(
 	@Param('productId', ParseIntPipe) productId: number,
+	@TransactionDecorator() transaction: Transaction,
   ): Promise<GetListProductPropertyResponse[]> {
 	return this.productPropertyService.getListProductProperty(productId);
   }
@@ -74,10 +84,12 @@ export class ProductPropertyController {
   @UseGuards(JwtGuard, RoleGuard)
   @HttpCode(200)
   @Put('/product_id/:productId([0-9]+)/update_property/:id([0-9]+)')
+  @UseInterceptors(TransactionInterceptor)
   public async updateProductProperty(
 	@Param('productId', ParseIntPipe) productId: number,
 	@Param('id', ParseIntPipe) id: number,
 	@Body() dto: CreateProductPropertyDto,
+	@TransactionDecorator() transaction: Transaction,
   ): Promise<UpdateProductPropertyResponse> {
 	return this.productPropertyService.updateProductProperty(
 		productId,
@@ -91,9 +103,11 @@ export class ProductPropertyController {
   @UseGuards(JwtGuard, RoleGuard)
   @HttpCode(200)
   @Delete('/product_id/:productId([0-9]+)/remove-product-property/:id([0-9]+)')
+  @UseInterceptors(TransactionInterceptor)
   public async removeProductProperty(
 	@Param('productId', ParseIntPipe) productId: number,
 	@Param('id', ParseIntPipe) id: number,
+	@TransactionDecorator() transaction: Transaction,
   ): Promise<RemoveProductPropertyResponse> {
 	return this.productPropertyService.removeProductProperty(productId, id);
   }
