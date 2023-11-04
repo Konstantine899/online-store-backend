66e34cb329164a9361f74e50546dd8ca624cf8e7
Konstantin Atroschenko <kostay375298918971@gmail.com>
2023-11-04 23:08:15 +0300

    feat: infrastructure/config/swagger добавил public api

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import { SequelizeUniqueConstraintExceptionFilter } from './infrastructure/excep
 import { CustomNotFoundExceptionFilter } from './infrastructure/exceptions/custom-not-found.exception.filter';
 import { SequelizeDatabaseErrorExceptionFilter } from './infrastructure/exceptions/sequelize-database-error.exception.filter';
 import * as cookieParser from 'cookie-parser';
-import { swaggerConfig } from './infrastructure/config/swagger/swagger.config';
+import { swaggerConfig } from '@app/infrastructure/config/swagger';
 
 async function bootstrap(): Promise<void> {
     const PORT = process.env.PORT || 5000;
@@ -31,9 +31,9 @@ async function bootstrap(): Promise<void> {
     });
     app.use(cookieParser(process.env.COOKIE_PARSER_SECRET_KEY));
     swaggerConfig(app);
-    await app.listen(PORT, () =>
-        console.log(`Server started on port = ${PORT}`),
-    );
+    await app.listen(PORT, () => {
+        return console.log(`Server started on port = ${PORT}`);
+    });
 }
 
 bootstrap();
